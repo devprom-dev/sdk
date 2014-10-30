471196c7a35964146499df827f2a05f63d37aa6e
saveug <saveug@mail.ru>
2014-10-30 11:16:29 +0300

    - добавить в intercom шаблоны используемых проектов

--- app/plugins/saasassist/classes/ScriptIntercomBuilder.php
+++ app/plugins/saasassist/classes/ScriptIntercomBuilder.php
@@ -19,14 +19,18 @@ class ScriptIntercomBuilder extends ScriptBuilder
     	
     	if ( $user_it->getId() < 1 ) return;
     	
+    	$project_it = getFactory()->getObject('Project')->getRegistry()->Query();
+    	
     	$content = file_get_contents(SERVER_ROOT_PATH."plugins/saasassist/resources/intercom.js");
     	
     	$content = preg_replace('/%name%/', IteratorBase::wintoutf8($user_it->getDisplayName()), $content);  
     	$content = preg_replace('/%email%/', $user_it->get('Email'), $content);
     	$content = preg_replace('/%date%/', (new DateTime($user_it->get('RecordCreated')))->getTimestamp(), $content);
-    	$content = preg_replace('/%projects%/', getFactory()->getObject('Project')->getRegistry()->Count(), $content);
     	$content = preg_replace('/%users%/', getFactory()->getObject('User')->getRegistry()->Count(), $content);
     	$content = preg_replace('/%host%/', EnvironmentSettings::getServerName(), $content);
+    	
+    	$content = preg_replace('/%projects%/', $project_it->count(), $content);
+    	$content = preg_replace('/%templates%/', join(',',$project_it->fieldToArray('Tools')), $content);
     	
     	$file_name = tempnam(sys_get_temp_dir(), 'intercom');
     	
