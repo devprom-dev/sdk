92ed67a286948ea9a5e1a17f8b4dece2c5c2d334
saveug <saveug@mail.ru>
2015-05-25 22:05:59 +0300

    http://hq.devprom.ru/pm/devtest/I-22614

--- app/plugins/accountclient/views/templates/failed.tpl.php
+++ app/plugins/accountclient/views/templates/failed.tpl.php
@@ -4,8 +4,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251"></meta>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="/cache/?v=<?=$current_version?>&type=css"/>
-	<script src="/cache/?v=<?=$current_version?>&l=<?=$language_code?>" type="text/javascript" charset="UTF-8"></script>
+	<link rel="stylesheet" type="text/css" href="/cache/?v=<?=$current_version?>&asset=1&type=css"/>
+	<link rel="stylesheet" type="text/css" href="/cache/?v=<?=$current_version?>&asset=2&type=css"/>
+	<script src="/cache/?v=<?=$current_version?>&asset=1&l=<?=$language_code?>" type="text/javascript" charset="UTF-8"></script>
+	<script src="/cache/?v=<?=$current_version?>&asset=2&l=<?=$language_code?>" type="text/javascript" charset="UTF-8"></script>
+	<script src="/cache/?v=<?=$current_version?>&asset=3&l=<?=$language_code?>" type="text/javascript" charset="UTF-8"></script>
   </head>
   <body style="background: white;margin: 10px 10px 0 10px;">
 	  	<?=text('accountclient3')?>
